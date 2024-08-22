d76e259979a4d78fb80291b87a61b64e6c88b11d
DragonWrangler1 <146014546+DragonWrangler1@users.noreply.github.com>
2024-08-22 16:09:03 -0700

    Add files via upload

--- init.lua
+++ init.lua
@@ -2,9 +2,7 @@ local modpath = minetest.get_modpath("vlf_falling_suprise")
 minetest.register_on_joinplayer(function(player)
 	local player_name = player:get_player_name()
 	if minetest.settings:get_bool("vlf_falling_destroy", false) then
-	local message = minetest.colorize("#FFFFFF", "<VoxelForge> Please Ensure that you're not using this in an old world. This will destroy it. If you are willing to accept that  this will destroy your world then you may type   -- /start true --")
-	error("<VoxelForge> Please Ensure that you're not using this in an old world. This will destroy it. If you are willing to accept that  this will destroy your world then you may type   please go to your settings and set it to true.")
-	minetest.chat_send_player(player_name, message)
+	error("<VoxelForge> Please Ensure that you're not using this in an old world. This will destroy it. If you are willing to accept that  this will destroy your world then you may type   please go to your settings and set it to true/Enabled.")
 	end
 end)
 
